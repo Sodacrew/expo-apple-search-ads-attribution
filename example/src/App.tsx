0870b6a179950905f888a4035199988c19658a18
Ethan <ethan.park@sodacrew.com>
2022-04-19 19:19:23 +0900

    Fix ios example error

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
+import { View } from 'react-native';
 import AppleAdsAttribution from '@sodacrew/expo-apple-search-ads-attribution';
 
 export default function App() {
@@ -11,22 +11,18 @@ export default function App() {
     });
   }, []);
 
-  return (
-    <View style={styles.container}>
-      <Text>Result: {5}</Text>
-    </View>
-  );
+  return <View />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   box: {
+//     width: 60,
+//     height: 60,
+//     marginVertical: 20,
+//   },
+// });
