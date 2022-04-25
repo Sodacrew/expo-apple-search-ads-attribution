bd04bcf9a2d96974fe4bf9dec9c64cfab495a254
Ethan <ethan.park@sodacrew.com>
2022-04-25 16:12:35 +0900

    remove lib in .gitignore

---
+++ lib/typescript/index.d.ts
@@ -0,0 +1,5 @@
+declare class AppleAdsAttribution {
+    getAttributionData(): Promise<import("./typings").ExpoAppleSearchAdAttributionData | null>;
+}
+declare const AppleAdsAttributionInstance: AppleAdsAttribution;
+export default AppleAdsAttributionInstance;
